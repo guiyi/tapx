c37bbb25baef23b34be8463df4576ff816e9eb84
adiar zou <adiar zou>
2016-06-23 19:34:17 +0800

    tapeixun

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'a0604083931';
+$cfg_dbuser = 'a0604083931';
+$cfg_dbpwd = '508faf46';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
